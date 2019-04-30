524048edfae43b6c3d64b10a0534675b823f6a42
Jimmy Satterfield <fedora.game.dev@gmail.com>
2019-04-29 22:19:10 -0700

    Express server now runable

---
+++ frontend/build/precache-manifest.84e7374c5a718450406a9f9d27a73da3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae62ebe7b7a819caf31a13c1440e971a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c948c5350653e6f1cbe9",
+    "url": "/static/css/main.ece1f429.chunk.css"
+  },
+  {
+    "revision": "21f4d1943a3d840e58a4",
+    "url": "/static/js/2.9449ff86.chunk.js"
+  },
+  {
+    "revision": "c948c5350653e6f1cbe9",
+    "url": "/static/js/main.8bab6ddd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "803c58018e54441bc0e5db707c373346",
+    "url": "/static/media/Chest.803c5801.png"
+  },
+  {
+    "revision": "f088fca30382bccf8ad3bc4b706513f9",
+    "url": "/static/media/Ireena.f088fca3.png"
+  },
+  {
+    "revision": "33e524fa4e2b7ec62389b3a4350378be",
+    "url": "/static/media/UnityLogo.33e524fa.svg"
+  },
+  {
+    "revision": "64f78189961636d7be15de696ed8b83d",
+    "url": "/static/media/firebase.64f78189.svg"
+  },
+  {
+    "revision": "59296ba10a723de7222690e501df0d94",
+    "url": "/static/media/simplyRoleplay.59296ba1.png"
+  }
+]);
